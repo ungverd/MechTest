26505b5136a1c0cd40fe26d2c9e2c0290b1d2611
ungverd <cfvhfyljvytav@mail.ru>
2019-08-15 20:51:58 +0300

    fix typo

--- retranslators_test.py
+++ retranslators_test.py
@@ -3,9 +3,9 @@ import random
 RETRANSLATORS = 200 # число люстр
 BUF_LEN = 10 # размер кольцевого буфера
 IDS = 15 # число разных возможных id
-NEIGH_NUM = 3 # метрика числа cocедей у люстры, на самом деле их получается примерно NEIGH_NUM*2 + 1
+NEIGH_NUM = 1 # метрика числа cocедей у люстры, на самом деле их получается примерно NEIGH_NUM*2 + 1
               # если NEIGH_NUM = 0, то соседей 2, люстры выстроены в кольцо
-TRANSMISSION_PROBABILITY = 0.99 # вероятность передачи между люстрами
+TRANSMISSION_PROBABILITY = 0.9 # вероятность передачи между люстрами
 MESSAGES_TO_SEND = 2000 # число сообщений, которые будут посланы за симуляцию
 NEW_MESSAGE_PROBABILITY = 1 # вероятность, что Ксотар отправит новое сообщение в эту итерацию
 
@@ -35,13 +35,13 @@ class Retranslator:
     def receive(self, Id, infinite_id):
         if Id not in self.buf:
             if infinite_id in self.infinite_buf:
-                print("false_received %d" % infinite_id)
+                print("!! false_received %d" % infinite_id)
             self.to_send.append((Id, infinite_id))
             self.buf.add(Id)
             self.infinite_buf.append(infinite_id)
         else:
             if infinite_id not in self.infinite_buf:
-                print("false not received %d" % infinite_id)
+                print("!! false not received %d" % infinite_id)
 
     def send_all(self, iteration):
         for ids in self.to_send:
@@ -101,28 +101,8 @@ def main():
                 if message not in lost_messages:
                     print("message lost", message)
                     lost_messages.append(message)
+    print("lost", len(lost_messages)*100/MESSAGES_TO_SEND, "% of messages")
     print("end")
-
-
-main()
-
-    infinite_id = 0
-    for i in range(ITERATIONS):
-        if random.random() < NEW_MESSAGE_PROBABILITY:
-            Id = infinite_id % IDS
-            startRet = retranslators[random.randint(0, len(retranslators)-1)]
-            res = startRet.send(Id, infinite_id)
-            if res:
-                print(res)
-                return
-            infinite_id += 1
-        for ret in retranslators:
-            res = ret.send_all(i)
-            if res:
-                print(res)
-                return
-    print("good")
-        
 
 
 main()
